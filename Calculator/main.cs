4c545406df0a0f33226ecb0b8048fa4d3860e77f
ducyen <duc_ttm@yahoo.com>
2017-01-11 08:30:51 +0900

    Created by Coding Ground Client

---
+++ Calculator/main.cs
@@ -0,0 +1,10 @@
+using Main;
+
+namespace SimpleCalculator {
+    class Program {
+        static void Main(string[] args) {
+            CalcController calc = new CalcController();
+            calc.Run();
+        }
+    }
+}
